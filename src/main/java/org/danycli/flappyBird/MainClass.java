6de0dc43166e89a917eecfb79edc963f75761078
danycli <angelgaming331@gmail.com>
2026-03-15 23:58:36 +0500

    Make Some Tweakings

--- src/main/java/org/danycli/flappyBird/MainClass.java
+++ src/main/java/org/danycli/flappyBird/MainClass.java
@@ -4,6 +4,6 @@ import javafx.application.Application;
 
 public class MainClass {
     public static void main(String[] args) {
-            Application.launch(EventHandler.class, args);
+        Application.launch(EventHandler.class, args);
     }
 }
